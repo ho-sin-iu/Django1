d6d2438f965125b5e2926d8dbd6ca4db67c61fbc
Sin-iu Ho <jb22621550@gmail.com>
2024-07-05 16:59:43 +0800

    Create a new function called 'index'

--- Django1/urls.py
+++ Django1/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from blog_post.views import index
 from myapp.views import sayhello, hello2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 	# path('', sayhello),
 	# path('hello', sayhello),
 	# re_path('^hello2/(\w+)/$', hello2),
