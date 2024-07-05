72abcf08cb617c2e0e86e1e3fd3f53def8eec5fd
Sin-iu Ho <jb22621550@gmail.com>
2024-07-05 14:56:10 +0800

    Create a new class called Guestbook in blog_post's models

--- blog_post/models.py
+++ blog_post/models.py
@@ -12,3 +12,17 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Guestbook(models.Model):
+    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+    req_time = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ['-req_time']
+    
+    def __str__(self):
+        return self.title
