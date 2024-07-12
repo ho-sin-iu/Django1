27f03bc2bfbab90a1e1d7a1d7baf6be95c76506e
Sin-iu Ho <jb22621550@email.com>
2024-07-12 09:51:33 +0800

    Update settings and requirements

--- Django1/settings.py
+++ Django1/settings.py
@@ -132,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
