10a769b0e20dac1392ffa3b40a248c600cf8ee4c
Sin-iu Ho <jb22621550@gmail.com>
2024-08-06 17:59:41 +0800

    Line_bot

--- Django1/urls.py
+++ Django1/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from blog_post.views import index, showPost, testjson
+from line_bot.views import callback
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('post/<slug:slug>', showPost),
     path('api/v1/test', testjson),
+    path('line/', callback),
 ]
